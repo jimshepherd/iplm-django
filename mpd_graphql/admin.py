216a11f0796f7bdc9c58c93d5977f64059d2590e
Jim Shepherd <jeshep@gmail.com>
2022-07-26 22:21:20 +0000

    Add Product Specification fields to admin

--- mpd_graphql/admin.py
+++ mpd_graphql/admin.py
@@ -1,12 +1,52 @@
+from django import forms
 from django.contrib import admin
-#from treebeard.admin import TreeAdmin
-#from treebeard.forms import movenodeform_factory
+from feincms3.admin import TreeAdmin
+import json
 
-from .models import Attribute
+
+from .models import \
+    Attribute, \
+    MaterialSpecification, MaterialType, \
+    PropertySpecification, PropertyType
+
+
+class PrettyJSONEncoder(json.JSONEncoder):
+    def __init__(self, *args, indent, sort_keys, **kwargs):
+        super().__init__(*args, indent=2, sort_keys=True, **kwargs)
+
 
 
 @admin.register(Attribute)
 class AttributeAdmin(admin.ModelAdmin):
     #form = movenodeform_factory(Attribute)
     list_display = ('name',)
+
+
+@admin.register(MaterialSpecification)
+class MaterialSpecificationAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(MaterialType)
+class MaterialTypeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(PropertyType)
+class PropertyTypeAdmin(TreeAdmin):
+    list_display = ('indented_title', 'move_column', 'name',)
+
+
+class PropertySpecificationForm(forms.ModelForm):
+    name = forms.CharField(max_length=100)
+    values = forms.JSONField(encoder=PrettyJSONEncoder)
+    unit = forms.CharField(max_length=50)
+
+
+@admin.register(PropertySpecification)
+class PropertySpecificationAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    readonly_fields = ('created_by', 'created_at', 'modified_by', 'modified_at',)
+    form = PropertySpecificationForm
+
 
